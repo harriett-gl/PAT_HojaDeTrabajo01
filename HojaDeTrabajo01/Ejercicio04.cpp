ceb76ec22468fe85c542219595b261041e0a7ec4
harriett-gl <144355411+harriett-gl@users.noreply.github.com>
2023-09-11 18:39:00 -0600

    Update Ejercicio04.cpp

--- HojaDeTrabajo01/Ejercicio04.cpp
+++ HojaDeTrabajo01/Ejercicio04.cpp
@@ -1,6 +1,21 @@
 #include "Ejercicio04.h"
 
+void generar(vector<string>& res, string s, int open, int close, int n) {
+    if (open == n && close == n) {
+        res.push_back(s);
+        return;
+    }
+    if (open < n) {
+        generar(res, s + "(", open + 1, close, n);
+    }
+    if (close < open) {
+        generar(res, s + ")", open, close + 1, n);
+    }
+}
+
 vector<string> Ejercicio04::generateParenthesis(int n)
 {
-    return vector<string>();
+    vector<string> res;
+    generar(res, "", 0, 0, n);
+    return res;
 }
