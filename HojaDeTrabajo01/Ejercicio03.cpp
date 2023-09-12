be0742d9071f6f9117b2004ef1cc4e46a00b247f
harriett-gl <144355411+harriett-gl@users.noreply.github.com>
2023-09-11 18:38:37 -0600

    Update Ejercicio03.cpp

--- HojaDeTrabajo01/Ejercicio03.cpp
+++ HojaDeTrabajo01/Ejercicio03.cpp
@@ -1,6 +1,17 @@
 #include "Ejercicio03.h"
+#include <cmath>
 
 bool Ejercicio03::isPowerOfTwo(int n)
 {
+    n = abs(n);
+    if (n == 0) {
+        return false;
+    }
+    if (n == 1) {
+        return true;
+    }
+    if (n % 2 == 0) {
+        return isPowerOfTwo(n / 2);
+    }
     return false;
 }
